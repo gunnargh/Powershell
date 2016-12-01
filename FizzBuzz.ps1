c07a3e99433c00e87004432f227a9b42e7320af6
Gunnar Geir Helgason <gunnargeir89@gmail.com>
2016-12-01 16:19:41 +0100

    :)

--- FizzBuzz.ps1
+++ FizzBuzz.ps1
@@ -1,14 +1,16 @@
 for($i = 1;$i -lt 100; $i++){
     $a = ""
+    $Res = ""
     $b = [int32]::TryParse( $i/3 , [ref]$a )
     if ($b -eq $true) {
-        Write-Host "Fizz"
+        $Res += "Fizz"
     }
     $e = [int32]::TryParse( $i/5 , [ref]$a )
     if ($e -eq $true) {
-        Write-Host "Buzz"
+        $Res += "Buzz"
     }
     if ($e -eq $false -and $b -eq $false) {
-        Write-Host $i
+        $Res = $i
     }
+    Write-Host $res
 }
